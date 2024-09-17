22637a01bec8b6d594028308b6d743be8ea44c89
FarooqQaiser <farooqqaiser679@gmail.com>
2024-09-17 22:51:19 +0500

    Voice Library file is commented out

--- src/routes/sidebar.js
+++ src/routes/sidebar.js
@@ -1,14 +1,14 @@
 /** Icons are imported separately to reduce build time */
-import DocumentTextIcon from "@heroicons/react/24/outline/DocumentTextIcon";
-import Squares2X2Icon from "@heroicons/react/24/outline/Squares2X2Icon";
+// import DocumentTextIcon from "@heroicons/react/24/outline/DocumentTextIcon";
+// import Squares2X2Icon from "@heroicons/react/24/outline/Squares2X2Icon";
 import UserIcon from "@heroicons/react/24/outline/UserIcon";
 import InboxArrowDownIcon from "@heroicons/react/24/outline/InboxArrowDownIcon";
-import UsersIcon from "@heroicons/react/24/outline/UsersIcon";
+// import UsersIcon from "@heroicons/react/24/outline/UsersIcon";
 import ChartPie from "@heroicons/react/24/outline/ChartPieIcon";
-import CpuChipIcon from "@heroicons/react/24/outline/CpuChipIcon";
+// import CpuChipIcon from "@heroicons/react/24/outline/CpuChipIcon";
 import PhoneIcon from "@heroicons/react/24/outline/PhoneIcon";
-import ToolsIcon from "@heroicons/react/24/outline/WrenchScrewdriverIcon";
-import MicrophoneIcon from "@heroicons/react/24/outline/MicrophoneIcon";
+// import ToolsIcon from "@heroicons/react/24/outline/WrenchScrewdriverIcon";
+// import MicrophoneIcon from "@heroicons/react/24/outline/MicrophoneIcon";
 
 const iconClasses = `h-6 w-6`;
 const submenuIconClasses = `h-5 w-5`;
@@ -54,11 +54,11 @@ const routes = [
   //   icon: <UsersIcon className={submenuIconClasses} />,
   //   name: "Squads",
   // },
-  {
-    path: "/voice-library",
-    icon: <MicrophoneIcon className={iconClasses} />,
-    name: "Voice Library",
-  },
+  // {
+  //   path: "/voice-library",
+  //   icon: <MicrophoneIcon className={iconClasses} />,
+  //   name: "Voice Library",
+  // },
   {
     path: "/call-logs",
     icon: <InboxArrowDownIcon className={iconClasses} />,
